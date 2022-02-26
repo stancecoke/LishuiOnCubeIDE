62dece4d18a5ed7ad0e7638a7ebf70bc75ec5c08
stancecoke <hochsitzcola@web.de>
2022-02-26 12:37:57 +0100

    adapted to Lishui pinout

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -17,48 +17,48 @@ extern "C" {
 #include "motor.h"
 
 // motor hall pins (will be used as input EXTI) (used by the motor control code)
-#define HALL_1_GPIO_PORT    GPIOB
-#define HALL_1_PIN          GPIO_PIN_4
-#define HALL_2_GPIO_PORT    GPIOB
-#define HALL_2_PIN          GPIO_PIN_5
-#define HALL_3_GPIO_PORT    GPIOB
-#define HALL_3_PIN          GPIO_PIN_0
+#define HALL_1_GPIO_PORT    GPIOA
+#define HALL_1_PIN          GPIO_PIN_0
+#define HALL_2_GPIO_PORT    GPIOA
+#define HALL_2_PIN          GPIO_PIN_1
+#define HALL_3_GPIO_PORT    GPIOA
+#define HALL_3_PIN          GPIO_PIN_2
 
 // motor ADC pins (used by the motor control code)
 #define MOTOR_PHASE_CURRENT_A_PORT  GPIOA
-#define MOTOR_PHASE_CURRENT_A_PIN   GPIO_PIN_3
+#define MOTOR_PHASE_CURRENT_A_PIN   GPIO_PIN_4
 #define MOTOR_PHASE_CURRENT_B_PORT  GPIOA
-#define MOTOR_PHASE_CURRENT_B_PIN   GPIO_PIN_4
+#define MOTOR_PHASE_CURRENT_B_PIN   GPIO_PIN_5
 #define MOTOR_PHASE_CURRENT_C_PORT  GPIOA
-#define MOTOR_PHASE_CURRENT_C_PIN   GPIO_PIN_5
+#define MOTOR_PHASE_CURRENT_C_PIN   GPIO_PIN_6
 
 // battery voltage ADC pin (used by the motor control code)
 #define BATTERY_VOLTAGE_PORT  GPIOA
-#define BATTERY_VOLTAGE_PIN   GPIO_PIN_2
+#define BATTERY_VOLTAGE_PIN   GPIO_PIN_7
 
 // throttle ADC pin (used by the user application code)
 #define THROTTLE_PORT   GPIOA
-#define THROTTLE_PIN    GPIO_PIN_7
+#define THROTTLE_PIN    GPIO_PIN_3
 
 // wheel speed sensor (used by the user application code)
 #define WHELL_SPEED_SENSOR_PORT   GPIOB
-#define WHELL_SPEED_SENSOR_PIN    GPIO_PIN_0
+#define WHELL_SPEED_SENSOR_PIN    GPIO_PIN_5
 
 
 #define DEBUG_PIN_CONFIG \
-  GPIO_InitStruct.Pin = GPIO_PIN_15; \
+  GPIO_InitStruct.Pin = GPIO_PIN_2; \
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; \
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW; \
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct)
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct)
 
 #define DEBUG_TOGGLE \
-  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_15) // DEBUG
+  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2) // DEBUG
 
 #define DEBUG_ON \
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, true) // DEBUG
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, true) // DEBUG
 
 #define DEBUG_OFF \
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, false) // DEBUG
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, false) // DEBUG
 
 void Error_Handler(void);
 
